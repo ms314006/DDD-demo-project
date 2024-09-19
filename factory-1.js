27007afe1e8283a7d7510f1354562ff2fb5268ff
GQSM <ms314006@hotmail.com>
2024-09-19 21:12:06 +0800

    UPDATE 於 getCarriedPokemon 中的建構增加 player 的 id

--- factory-1.js
+++ factory-1.js
@@ -1,6 +1,7 @@
 class Player {
   getCarriedPokemon(carriedPokemonNo) {
     return new CarriedPokemon({
+      playerId: this.id,
       no: carriedPokemonNo,
       id: this.pokemons[carriedPokemonNo].id,
     });
